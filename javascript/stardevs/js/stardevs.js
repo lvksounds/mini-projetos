51cd74514d8c0d7ea68738490323bf3dcfd78a37
lucas.freitas <lvksounds@gmail.com>
2022-07-28 17:08:47 -0300

    finalizando fetchs

--- javascript/stardevs/js/stardevs.js
+++ javascript/stardevs/js/stardevs.js
@@ -7,7 +7,13 @@ fillCounters();
 function fillCounters() {
   Promise.all([getData("people/"), getData("starships/"), getData("planets/")])
     .then((data) => {
-      console.log(data);
+      persons.style.fontSize = "5rem";
+      starships.style.fontSize = "5rem";
+      planets.style.fontSize = "5rem";
+
+      persons.innerHTML = data[0].count;
+      starships.innerHTML = data[1].count;
+      planets.innerHTML = data[2].count;
     })
     .catch((err) => {
       console.log("Erro: ", err);
@@ -16,4 +22,19 @@ function fillCounters() {
 
 function getData(param) {
   return fetch(`https://swapi.dev/api/${param}`).then((res) => res.json());
+}
+
+function loadPhrase() {
+  const phrase = document.getElementById("phrase");
+  const btnphrases = document.getElementById("btn-phrases");
+  return fetch(
+    "https://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote"
+  )
+    .then((data) => data.json())
+    .then((json) => {
+      console.log(json);
+      btnphrases.innerHTML = "Ver mais uma frase!";
+      phrase.innerHTML = "";
+    })
+    .catch((err) => console.log("erro: ", err));
 }
