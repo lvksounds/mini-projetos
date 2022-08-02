7adf6e69125ecfcebf88d799ebd397cd8e1b9062
lucas.freitas <lvksounds@gmail.com>
2022-08-02 09:58:46 -0300

    Finalização do Relógio em JS

--- javascript/clock/clock.js
+++ javascript/clock/clock.js
@@ -1,6 +1,17 @@
-// const dataTxt = document.getElementById("data");
+const getHours = () => {
+  const clock = document.getElementsByClassName("clock")[0];
+  const data = new Date();
 
-let data = new Date("August 01, 2022 08:46:30");
-let day = data.getDate();
+  const horas = data.getHours();
+  const minutos = data.getMinutes();
+  const segundos = data.getSeconds();
 
-console.log(day);
+  const hora = horas < 10 ? `0${horas}` : horas;
+  const minuto = minutos < 10 ? `0${minutos}` : minutos;
+  const segundo = segundos < 10 ? `0${segundos}` : segundos;
+  clock.innerHTML = `${hora}:${minuto}:${segundo}`;
+};
+
+setInterval(() => {
+  getHours();
+}, 1000);
